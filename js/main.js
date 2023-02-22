1c0ba5f54e0bc84c4320ff7c7bdd1e6816b1699e
ldemars <ldemars@wisc.edu>
2023-02-22 03:03:08 -0600

    activity-5 commit 2
    Added mortality data and changed code so it is read by javascript main file.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var map;
 function createMap(){
 
     //create the map
-    map = L.map('map', {
+    map = L.map('map', { //Uses id map for use in html
         center: [0, 0],
         zoom: 2
     });
@@ -20,28 +20,28 @@ function createMap(){
 };
 
 //function to retrieve the data and place it on the map
-//added at Example 2.3 line 20...function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
+//function to attach popups to each mapped feature
+function onEachFeature(feature, layer) { //Passes in variables "feature" and "layer" from geojson data.
     //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
+    var popupContent = ""; //Initializes popupContent
     if (feature.properties) {
         //loop to add feature property names and values to html string
-        for (var property in feature.properties){
+        for (var property in feature.properties){ //Loop is performed for each item stored in the GeoJSON data.
             popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
         }
-        layer.bindPopup(popupContent);
+        layer.bindPopup(popupContent); //Binds information as popup content.
     };
 };
 
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data
-    fetch("data/MegaCities.geojson")
+    fetch("data/NatMortalityRateUnder5.geojson")
         .then(function(response){
             return response.json();
         })
-        .then(function(json){
-            var geojsonMarkerOptions = {
+        .then(function(json){ 
+            var geojsonMarkerOptions = { //Creates characteristics of polygons to replace markers.
                 radius: 8,
                 fillColor: "#ff7800",
                 color: "#000",
@@ -53,12 +53,12 @@ function getData(map){
             //create a Leaflet GeoJSON layer and add it to the map
             
             L.geoJson(json, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                pointToLayer: function (feature, latlng){ //Converts point features to polygon layer
+                    return L.circleMarker(latlng, geojsonMarkerOptions); //Specifies circleMarker as polygon type, using variables set above in geojsonMarkerOptions.
                 },
-                onEachFeature: onEachFeature
-            }).addTo(map);
+                onEachFeature: onEachFeature //Sends data to onEachFeature, to implement data into each marker upon clicking them.
+            }).addTo(map); //Adds circle markers with linked data to the final map.
         })  
 };
 
-document.addEventListener('DOMContentLoaded',createMap)
+document.addEventListener('DOMContentLoaded',createMap) //Waits for each process to finish while running createMap.
